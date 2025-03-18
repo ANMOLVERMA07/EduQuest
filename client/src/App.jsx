952f80afc05d14919af3118cc206c69730d0eb6b
Anmol Verma <153612130+ANMOLVERMA07@users.noreply.github.com>
2025-03-18 16:53:28 +0000

    Frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Routes, Route } from "react-router-dom"
+
+const App = () => {
+  return (
+    <>
+      <div>App</div>
+
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </>
+  )
+}
+
+export default App
